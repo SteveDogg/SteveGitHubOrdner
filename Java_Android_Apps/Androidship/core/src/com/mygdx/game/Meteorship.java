3708c4fa5093e25c70d8dce92d840be80aef9169
SteveDogg <stefansim2000@gmail.com>
2018-02-23 17:21:37 +0100

    Ergo

--- Java_Android_Apps/Androidship/core/src/com/mygdx/game/Meteorship.java
+++ Java_Android_Apps/Androidship/core/src/com/mygdx/game/Meteorship.java
@@ -6,19 +6,45 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+///
+///Stefan Simic
+///Meteorship
+///23.02.2018
+///Version 1.0
+///
+
 public class Meteorship extends ApplicationAdapter {
 	SpriteBatch batch;
-	
+	Texture img;
+
+	//Dateitypen werden erstellt, aber noch nicht zugewiesen
+
+
+
+	//Dateitypen werden erstellt und gleich zugewiesen
+
+	int intKoordinateX; //Koordinaten des Fingers X
+	int intKoordinateY; //Koordinaten des Fingers Y
+
+
+	//OnCreate
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 
 		batch.begin();
+		MotionShip();
 		batch.end();
+
+	}
+
+	public void MotionShip () {
+		batch.draw(img, 0,0);
 	}
 
 }
